86d01cc1d2b1a0e4cd297fed611a4992de55add5
TheExtraOne <katya_mihasewa@mail.ru>
2023-01-21 22:40:50 +0300

    feat: prepared firebase, implemented form funstion for post requests

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,27 @@
 import NewMeetupForm from "../../components/meetups/new-meetup-form";
+import { useRouter } from "next/router";
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = (enteredMeetupData) => {
-    console.log(enteredMeetupData);
-  };
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch(
+      "https://meetings-handler-default-rtdb.europe-west1.firebasedatabase.app/meetings.json",
+      {
+        method: "POST",
+        body: JSON.stringify(enteredMeetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push("/");
+  }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 };
